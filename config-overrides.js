bd755ad8f8dd7d32ba19582c79f376be297205e8
bietkul <kuldepsaxena155@gmail.com>
2020-10-26 22:19:57 +0530

    fix: build output issues

--- config-overrides.js
+++ config-overrides.js
@@ -4,8 +4,15 @@ const {
   } = require("customize-cra");
 
 const publicPathPlugin = (config, env) => {
+    config.optimization.splitChunks = {
+        cacheGroups: {
+            default: false,
+        },
+    };
+    config.optimization.runtimeChunk = false;
     config.output.filename = 'static/js/[name].js';
-    config.plugins[4].filename = 'static/css/[name].css';
+    config.plugins[5].options.filename = 'static/css/[name].css';
+    config.plugins[5].options.chunkFilename = 'static/css/[name].css';
     return config
 }
 
