0ad55b3affbed32c631f436c44544a041ff50d36
bietkul <kuldepsaxena155@gmail.com>
2020-10-26 21:54:10 +0530

    fix: build output

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,18 @@ const {
     fixBabelImports,
   } = require("customize-cra");
 
+const publicPathPlugin = (config, env) => {
+    config.output.filename = 'static/js/[name].js';
+    config.plugins[4].filename = 'static/css/[name].css';
+    return config
+}
+
 module.exports = override(
     fixBabelImports("import", {
         libraryName: "antd", libraryDirectory: "es", style: 'css' // change importing css to less
     }),
     fixBabelImports("direct-import", {
         name: "@appbaseio/reactivesearch", indexFile: "@appbaseio/reactivesearch/lib/index.es.js"
-    })
+    }),
+    publicPathPlugin,
 )
