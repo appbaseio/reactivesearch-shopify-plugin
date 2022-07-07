8c0cbca9717d53eeef082d6fb811a80d5878a417
lohi212 <suvvarilohita@gmail.com>
2022-07-07 19:55:30 +0530

    remove committed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import get from 'lodash.get';
 import SearchPlugin from './components/SearchPlugin';
 import { getSearchPreferences } from './utils';
@@ -34,11 +29,6 @@ const App = ({ id }) => {
     return Object.keys(pageSettings).length ? (
         <Router>
             <Switch>
-                {/* <Redirect
-                    exact
-                    from="/"
-                    to={`${pageSettings.cuurentRoute} ? ${pageSettings.cuurentRoute} : '/`}
-                /> */}
                 <Route path="/dashboard">
                     <div>Dashboard Page</div>
                 </Route>
