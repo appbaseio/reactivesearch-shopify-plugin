46994b0aa58b619cd071ee8236185e7ba2fa8753
lohi212 <suvvarilohita@gmail.com>
2022-09-29 17:27:53 +0530

    feat: highlight sponsored results

--- src/components/Search.js
+++ src/components/Search.js
@@ -461,11 +461,10 @@ class Search extends Component {
             this.pageSettings,
             `pages.${this.pageSettings.currentPage}.indexSettings.fusionSettings`,
         );
-        const fusionSettings = Object.assign(
-            {},
-            globalFusionSettings,
-            pageFusionSettings,
-        );
+        const fusionSettings = {
+            ...globalFusionSettings,
+            ...pageFusionSettings,
+        };
         const endpoint = pageEndpoint || globalEndpoint;
         const mapsAPIkey = get(
             this.resultSettings,
